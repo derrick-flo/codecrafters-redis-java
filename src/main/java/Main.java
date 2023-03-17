7703116e9f0ed7c34031b4e3b2b7e7ac9ec42588
derrick.lee <derrick.lee@dreamus.io>
2023-03-17 11:16:57 +0900

    pass 3st stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,36 +1,46 @@
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 
 public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
+    public static void main(String[] args) {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.out.println("Logs from your program will appear here!");
 
-    //  Uncomment this block to pass the first stage
+        //  Uncomment this block to pass the first stage
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
         int port = 6379;
         try {
-          serverSocket = new ServerSocket(port);
-          serverSocket.setReuseAddress(true);
-          // Wait for connection from client.
-          clientSocket = serverSocket.accept();
+            serverSocket = new ServerSocket(port);
+            serverSocket.setReuseAddress(true);
+            // Wait for connection from client.
+            clientSocket = serverSocket.accept();
+            byte[] byteArr = new byte[100];
+            InputStream inputStream = clientSocket.getInputStream();
+            int readByteCount = inputStream.read(byteArr);
+            String data = new String(byteArr, 0, readByteCount, "UTF-8");
+
+            System.out.println(data);
+
             final OutputStream outputStream = clientSocket.getOutputStream();
-            final byte[] bytes = "+PONG\r\n".getBytes(StandardCharsets.UTF_8);
+            final byte[] bytes = "$0\r\n\r\n*2\r\n$4\r\nPONG\r\n$4\r\nPONG\r\n".getBytes(StandardCharsets.UTF_8);
+//            final byte[] bytes = "$0\r\n\r\n".getBytes(StandardCharsets.UTF_8);
             outputStream.write(bytes);
         } catch (IOException e) {
-          System.out.println("IOException: " + e.getMessage());
-        } finally {
-          try {
-            if (clientSocket != null) {
-              clientSocket.close();
-            }
-          } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
-          }
+        } finally {
+            try {
+                if (clientSocket != null) {
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
         }
-  }
+    }
 }
