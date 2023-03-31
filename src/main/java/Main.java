ac23a008f4b533364b8b0263e6dd8029c7542756
derrick.lee <derrick.lee@dreamus.io>
2023-03-31 16:21:58 +0900

    44444444444444444444

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Timer;
+import java.net.SocketException;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,21 +12,21 @@ public class Main {
         System.out.println("Logs from your program will appear here!");
 
         //  Uncomment this block to pass the first stage
-        ServerSocket serverSocket = null;
-        Socket clientSocket = null;
+        ServerSocket serverSocket;
         int port = 6379;
         try {
             serverSocket = new ServerSocket(port);
             serverSocket.setReuseAddress(true);
             // Wait for connection from client.
             while (true) {
-                clientSocket = serverSocket.accept();
+                Socket clientSocket = serverSocket.accept();
                 BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                 OutputStream outputStream = clientSocket.getOutputStream();
-                new Thread(() -> {
+
+                final Thread thread = new Thread(() -> {
                     try {
                         String line;
-                        while((line = in.readLine()) != null) {
+                        while ((line = in.readLine()) != null) {
                             System.out.println("just debug : " + line);
                             if ("ping".equalsIgnoreCase(line)) {
                                 outputStream.write("+PONG\r\n".getBytes());
@@ -34,20 +37,28 @@ public class Main {
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
-                }).run();
-                clientSocket.close();
+                    try {
+                        clientSocket.close();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
+                thread.start();
+                System.out.println("----end----");
             }
 
+        } catch (SocketException e) {
+            throw new RuntimeException(e);
         } catch (IOException e) {
-            System.out.println("IOException: " + e.getMessage());
+            throw new RuntimeException(e);
         } finally {
-            try {
-                if (clientSocket != null) {
-                    clientSocket.close();
-                }
-            } catch (IOException e) {
-                System.out.println("IOException: " + e.getMessage());
-            }
+//            try {
+//                if (clientSocket != null) {
+//                    clientSocket.close();
+//                }
+//            } catch (IOException e) {
+//                System.out.println("IOException: " + e.getMessage());
+//            }
         }
     }
 }
