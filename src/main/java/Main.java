64b6a6a9d0e2d089522f92f5d66a626048fa8e87
derrick.lee <derrick.lee@dreamus.io>
2023-04-07 01:53:31 +0900

    대충

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,12 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Main {
 
-    public static Map<String, String> memory = new HashMap<>();
-
+    private static final Map<String, ValueWithOptions> MEMORY = new ConcurrentHashMap<>();
 
     public static void main(String[] args) throws Exception {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -30,35 +29,66 @@ public class Main {
 
                 Thread thread = new Thread(() -> {
                     boolean beforeExistsCommand = false;
-                    boolean beforeSetCommand = false;
                     boolean beforeGetCommand = false;
+                    boolean beforeSetCommand = false;
+                    boolean beforePxCommand = false;
+                    int commandCount = 0;
                     String key = null;
+                    Long px = null;
+
                     try {
                         String line;
                         while ((line = in.readLine()) != null) {
                             System.out.println("just debug : " + line);
 
+                            if (line.startsWith("*")) {
+                                commandCount = Integer.valueOf(line.substring(1));
+                            }
                             if (line.equalsIgnoreCase("GET")) {
                                 beforeGetCommand = true;
                             } else if (beforeGetCommand && line.startsWith("$")) {
                             } else if (beforeGetCommand) {
-                                final String res = memory.get(line);
+                                final ValueWithOptions res = MEMORY.get(line);
                                 beforeGetCommand = false;
+                                commandCount = 0;
                                 if (res != null) {
-                                    String realRes = "+" + res + "\r\n";
+                                    if (isExpired(res.expiredTime, res.setTime)) {
+                                        MEMORY.remove(line);
+                                        outputStream.write("$-1\r\n".getBytes());
+                                        continue;
+                                    }
+                                    String realRes = "+" + res.value + " " + res.expiredTime + " " + res.setTime + "\r\n";
                                     outputStream.write(realRes.getBytes());
                                 } else {
                                     outputStream.write("$-1\r\n".getBytes());
                                 }
                             } else if (line.equalsIgnoreCase("SET")) {
                                 beforeSetCommand = true;
+                            } else if (line.equalsIgnoreCase("PX")) {
+                                beforePxCommand = true;
                             } else if (beforeSetCommand && line.startsWith("$")) {
+                            } else if (beforePxCommand && line.startsWith("$")) {
                             } else if (beforeSetCommand && key == null) {
                                 key = line;
-                            } else if (beforeSetCommand && key != null) {
-                                final String res = memory.put(key, line);
+                            } else if (beforePxCommand && px == null) {
+                                px = Long.valueOf(line);
+                            }
+
+                            if (beforePxCommand && px != null && commandCount == 5) {
+                                ValueWithOptions valueWithOptions = new ValueWithOptions(key, px, System.currentTimeMillis());
+                                MEMORY.put(key, valueWithOptions);
+                                beforePxCommand = false;
                                 beforeSetCommand = false;
                                 key = null;
+                                px = null;
+                                commandCount = 0;
+                                outputStream.write("+OK\r\n".getBytes());
+                            } else if (beforeSetCommand && key != null && commandCount == 3) {
+                                ValueWithOptions valueWithOptions = new ValueWithOptions(key, null, System.currentTimeMillis());
+                                MEMORY.put(key, valueWithOptions);
+                                beforeSetCommand = false;
+                                key = null;
+                                commandCount = 0;
                                 outputStream.write("+OK\r\n".getBytes());
                             } else if ("echo".equalsIgnoreCase(line)) {
                                 beforeExistsCommand = true;
@@ -66,10 +96,13 @@ public class Main {
                             } else if (!"echo".equalsIgnoreCase(line) && beforeExistsCommand) {
                                 String res = "+" + line + "\r\n";
                                 beforeExistsCommand = false;
+                                commandCount = 0;
                                 outputStream.write(res.getBytes());
                             } else if ("ping".equalsIgnoreCase(line)) {
+                                commandCount = 0;
                                 outputStream.write("+PONG\r\n".getBytes());
                             } else if ("DOCS".equalsIgnoreCase(line)) {
+                                commandCount = 0;
                                 outputStream.write("+\r\n".getBytes());
                             }
                         }
@@ -83,6 +116,24 @@ public class Main {
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private static boolean isExpired(final Long expiredTime, final Long setTime) {
+        long diffCurrentAndSetTime = System.currentTimeMillis() - setTime;
+
+        return expiredTime != null && expiredTime <= diffCurrentAndSetTime;
+    }
+
+    static class ValueWithOptions {
+        private String value;
+        private Long expiredTime;
+        private Long setTime;
+
+        public ValueWithOptions(final String value, final Long expiredTime, final Long setTime) {
+            this.value = value;
+            this.expiredTime = expiredTime;
+            this.setTime = setTime;
         }
     }
 }
